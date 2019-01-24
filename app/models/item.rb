5b75d62aa7138843c43fac96cf5e686bd9f52964
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-01-24 02:04:31 -0700

    solidify relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   validates_presence_of :name, :description, :unit_price
 
   belongs_to :merchant
-  belongs_to :customer
   has_many :invoice_items
   has_many :items, through: :invoice_items
 end
