474bef2584eca73acb77004fc87d28e38f77214f
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-01-26 15:47:09 -0700

    updated class method for transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,8 @@
 class Transaction < ApplicationRecord
   validates_presence_of :credit_card_number, :credit_card_expiration_date, :result
-
   belongs_to :invoice
+
+  def self.successful
+    where(result: 'success')
+  end
 end
